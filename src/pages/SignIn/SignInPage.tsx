3d6111c5207faa729eaabaa400d3125b34743c38
Adam S <adam.it.school.js@gmail.com>
2023-09-17 00:01:34 +0200

    refactoring code

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -1,143 +1,87 @@
 import React, { FC } from "react";
-import {
-  useForm,
-  Controller,
-  SubmitHandler,
-  FieldValues,
-} from "react-hook-form";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import ANBBQ from "../../assets/icons/ANBBQ.png";
 import Copyright from "../../assets/icons/Copyright.svg";
 import loginBg from "../../assets/images/login-bg.jpg";
 
+interface FormValues {
+  phoneNumber: string;
+  password: string;
+}
+
 const SignInPage: FC<{}> = () => {
-  const { handleSubmit, control } = useForm();
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const { handleSubmit, control, formState } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
   };
 
   return (
     <div
-      className="flex justify-center"
+      className="flex flex-col items-center bg-black h-full w-full bg-cover bg-center"
       style={{ backgroundImage: `url(${loginBg})` }}
     >
-      <div className="flex items-center bg-cover bg-center">
-        {/* <img className="w-full h-full" src={Wallpaper} alt="Wallpaper" />
-        <img className="w-full h-full" src={Wallpaper1} alt="Wallpaper1" /> */}
-        <div
-          className="absolute left-[562px] top-[148px] w-[508px] h-[621px]  bg-white"
-          style={{ borderRadius: "50px" }}
-        >
-          <img
-            className="w-[438px] h-[211px] ml-[29px] mt-[4px]"
-            src={ANBBQ}
-            alt="ANBBQ"
+      <form className="bg-white w-[500px] rounded-[50px] mt-[148px] p-4" onSubmit={handleSubmit(onSubmit)}>
+        <img className="w-[438px] h-[211px] ml-[29px] mt-[4px]" src={ANBBQ} alt="ANBBQ" />
+        <div>LOGIN</div>
+        <div className="mb-4 flex">
+          <label htmlFor="phoneNumber" className="mr-2 ml-[25px] mt-[53px] text-[#333] font-bold whitespace-nowrap">
+            Phone number
+          </label>
+          <Controller
+            name="phoneNumber"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <input
+                {...field}
+                id="phoneNumber"
+                type="text"
+                className="w-[342px] h-[38px] mt-[54px] mr-[27px] rounded border border-gray-300"
+              />
+            )}
           />
-          <div
-            style={{
-              fontSize: "40px",
-              color: "#0A376E",
-              fontFamily: "alegreya",
-              width: "119px",
-              height: "54px",
-              marginLeft: "195px",
-              marginTop: "42px",
-              lineHeight: "normal",
-              fontWeight: 900,
-              fontStyle: "normal",
-            }}
-          >
-            LOGIN
+        </div>
+
+        <div className="mb-4 flex">
+          <label htmlFor="password" className="w-[123px] h-[38px] ml-[25px] mt-[18px] text-[#333] font-bold">
+            Password
+          </label>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <input
+                {...field}
+                id="password"
+                type="password"
+                className="w-[342px] h-[38px] mt-[18px] mr-[27px] rounded border border-gray-300"
+              />
+            )}
+          />
+        </div>
+
+        <div className="flex mb-4">
+          <div className="w-[160px] h-[33px] ml-[139px] mr-[81px]">
+            <label>
+              <input type="checkbox" />
+              <span className="ml-[5px] mt-[17px]">Remember me</span>
+            </label>
           </div>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4 flex">
-              <div
-                className="mr-2 ml-[25px] mt-[53px] p-[0px] text-[#333] font-bold font-family: Noto Sans SC"
-                style={{ whiteSpace: "nowrap" }}
-              >
-                Phone number
-              </div>
-              <Controller
-                name="phoneNumber"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="text"
-                    className="w-[342px] h-[38px] mt-[54px] mr-[27px] rounded border border-gray-300"
-                    style={{ borderRadius: "20px" }}
-                  />
-                )}
-              />
-            </div>
-
-            <div className="mb-4 flex">
-              <div className="pr-[0px] w-[123px] h-[38px] ml-[25px] mt-[18px] text-[#333] font-bold">
-                Password
-              </div>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="password"
-                    className="pl-[0px] w-[342px] h-[38px] mt-[18px] mr-[27px] rounded border border-gray-300"
-                    style={{ borderRadius: "20px" }}
-                  />
-                )}
-              />
-            </div>
-            <div className="flex">
-              <div className="w-[160px] h-[33px] mb-4 ml-[139px] mr-[81px]">
-                <label>
-                  <input type="checkbox" />
-                  <span className="ml-[5px] mt-[17px]">Remember me</span>
-                </label>
-              </div>
-
-              <div className="mb-4 w-[187px] h-[33px]">
-                <a
-                  href="#"
-                  style={{ textDecoration: "underline", fontSize: "15px" }}
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-[123px] h-[41px] ml-[199px] mb-[35px] bg-blue-500 text-white px-4 py-2 rounded"
-                style={{ borderRadius: "50px" }}
-              >
-                <span
-                  className="w-full h-full flex justify-center items-center"
-                  style={{
-                    fontSize: "25px",
-                    fontWeight: "500",
-                    fontStyle: "normal",
-                  }}
-                >
-                  LOGIN
-                </span>
-              </button>
-            </div>
-          </form>
+          <div className="w-[187px] h-[33px]">
+            <a href="#" style={{ textDecoration: "underline", fontSize: "15px" }}>
+              Forgot password?
+            </a>
+          </div>
         </div>
-        <div
-          style={{
-            position: "absolute",
-            marginLeft: "630px",
-            marginTop: "970px",
-            textAlign: "center",
-          }}
+        <button
+          type="submit"
+          className="w-[123px] h-[41px] ml-[199px] mb-[35px] bg-blue-500 text-white px-4 py-2 rounded"
         >
-          <img className="w-[352px] h-[34px]" src={Copyright} alt="Copyright" />
-        </div>
-      </div>
+          LOGIN
+        </button>
+      </form>
+      <img className="w-[352px] h-[34px] mt-[180px]" src={Copyright} alt="Copyright" />
     </div>
   );
 };
