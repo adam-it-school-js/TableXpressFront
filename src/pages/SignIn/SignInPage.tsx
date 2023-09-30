d4914087d8cbc4189a5830c5ff9420daee966eae
Abuzar Aliev <aliev.amrrr@gmail.com>
2023-09-30 19:21:40 +0200

    tablexpress-front-react-4 refactored the code

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -3,6 +3,7 @@ import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import ANBBQ from "../../assets/icons/ANBBQ.png";
 import Copyright from "../../assets/icons/Copyright.svg";
 import loginBg from "../../assets/images/login-bg.jpg";
+import LOGIN from "../../assets/images/LOGIN.svg";
 
 interface FormValues {
   phoneNumber: string;
@@ -17,14 +18,28 @@ const SignInPage: FC<{}> = () => {
 
   return (
     <div
-      className="flex flex-col items-center bg-black h-full w-full bg-cover bg-center"
+      className="flex flex-col items-center bg-black h-[1052px] w-full bg-cover bg-center"
       style={{ backgroundImage: `url(${loginBg})` }}
     >
-      <form className="bg-white w-[500px] rounded-[50px] mt-[148px] p-4" onSubmit={handleSubmit(onSubmit)}>
-        <img className="w-[438px] h-[211px] ml-[29px] mt-[4px]" src={ANBBQ} alt="ANBBQ" />
-        <div>LOGIN</div>
+      <form
+        className="bg-white w-[508px] h-[619px] rounded-[50px] mt-[148px] p-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <img
+          className="w-[438px] h-[211px] ml-[29px] mt-[4px]"
+          src={ANBBQ}
+          alt="ANBBQ"
+        />
+        <img
+          className="w-[119px] h-[54px] ml-[195px] mt-[42px]"
+          src={LOGIN}
+          alt="LOGIN"
+        />
         <div className="mb-4 flex">
-          <label htmlFor="phoneNumber" className="mr-2 ml-[25px] mt-[53px] text-[#333] font-bold whitespace-nowrap">
+          <label
+            htmlFor="phoneNumber"
+            className="mr-2 ml-[25px] mt-[53px] text-[#333] font-bold whitespace-nowrap"
+          >
             Phone number
           </label>
           <Controller
@@ -36,14 +51,17 @@ const SignInPage: FC<{}> = () => {
                 {...field}
                 id="phoneNumber"
                 type="text"
-                className="w-[342px] h-[38px] mt-[54px] mr-[27px] rounded border border-gray-300"
+                className="w-[342px] h-[38px] mt-[54px] mr-[27px] rounded-[20px] border-[1px] border-gray-300"
               />
             )}
           />
         </div>
 
         <div className="mb-4 flex">
-          <label htmlFor="password" className="w-[123px] h-[38px] ml-[25px] mt-[18px] text-[#333] font-bold">
+          <label
+            htmlFor="password"
+            className="w-[123px] h-[38px] ml-[25px] mt-[18px] mr-[12px] text-[#333] font-bold"
+          >
             Password
           </label>
           <Controller
@@ -55,33 +73,45 @@ const SignInPage: FC<{}> = () => {
                 {...field}
                 id="password"
                 type="password"
-                className="w-[342px] h-[38px] mt-[18px] mr-[27px] rounded border border-gray-300"
+                className="w-[342px] h-[38px] mt-[18px] mr-[27px] rounded-[20px] border-[1px] border-gray-300"
               />
             )}
           />
         </div>
 
         <div className="flex mb-4">
-          <div className="w-[160px] h-[33px] ml-[139px] mr-[81px]">
+          <div className="w-[160px] h-[33px] ml-[139px] mr-[35px]">
             <label>
               <input type="checkbox" />
-              <span className="ml-[5px] mt-[17px]">Remember me</span>
+              <span
+                className="ml-[5px] mt-[17px]"
+                style={{ whiteSpace: "nowrap" }}
+              >
+                Remember me
+              </span>
             </label>
           </div>
-          <div className="w-[187px] h-[33px]">
-            <a href="#" style={{ textDecoration: "underline", fontSize: "15px" }}>
+          <div className="w-[187px] h-[33px] ml-[23px]">
+            <a
+              href="#"
+              style={{ textDecoration: "underline", fontSize: "15px" }}
+            >
               Forgot password?
             </a>
           </div>
         </div>
         <button
           type="submit"
-          className="w-[123px] h-[41px] ml-[199px] mb-[35px] bg-blue-500 text-white px-4 py-2 rounded"
+          className="flex justify-center items-center w-[123px] h-[41px] ml-[199px] mb-[35px] bg-blue-500 text-white px-4 py-2 rounded-[50px] text-[25px]"
         >
-          LOGIN
+          <div className="w-[83px] h-[30px] mt-[-5px]">LOGIN</div>
         </button>
       </form>
-      <img className="w-[352px] h-[34px] mt-[180px]" src={Copyright} alt="Copyright" />
+      <img
+        className="w-[352px] h-[34px] mt-[180px]"
+        src={Copyright}
+        alt="Copyright"
+      />
     </div>
   );
 };
